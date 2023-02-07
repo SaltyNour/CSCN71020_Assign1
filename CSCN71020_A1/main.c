b94cea9eb57decbe7c360289a691ce379fe56169
SaltyNour <nhamod9255@conestogac.on.ca>
2023-02-07 16:58:56 -0500

    add() now print all three numbers from the user and the result.

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -61,7 +61,7 @@ void add() {
 		scanf_s("%lf", &num2);
 		printf("Enter the third value:");
 		scanf_s("%lf", &num3);
-	    result = num1 - num2-num3;
+	    result = num1 - num2 - num3;
 	   printf("%lf - %lf - %lf = %lf\n", num1, num2, num3, result);
 
 }
